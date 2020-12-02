725a905d0e3aa462ff55b15be8bac795522a2b94
EstebanP <epinczinger@gmail.com>
2020-12-02 12:04:58 -0300

    add form and fetch search

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ const p = document.createElement('p');
 p.textContent = 'Hola';
 
 div.appendChild(p);
+
+const apiKey = 'f8584d3f19d9882aec61e1e892543fe7';
+const city = 'tokyo'
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const search = () => {
+    return fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=las+toninas&units=metric&appid=f8584d3f19d9882aec61e1e892543fe7`
+    ).then((response) => response.json());
+};
+
+search();
