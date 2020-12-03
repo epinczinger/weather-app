2733bffa74fb90aed7c7c1abe58b30aeeaa197a7
EstebanP <epinczinger@gmail.com>
2020-12-03 08:29:20 -0300

    search function return temp

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ p.textContent = 'Hola';
 
 div.appendChild(p);
 
-const apiKey = 'f8584d3f19d9882aec61e1e892543fe7';
-const city = 'tokyo'
-
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-const search = () => {
-    return fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=las+toninas&units=metric&appid=f8584d3f19d9882aec61e1e892543fe7`
-    ).then((response) => response.json());
+async function search(city, units='metric') {
+    let response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=f8584d3f19d9882aec61e1e892543fe7`
+    )
+    if (response.status == 200) {
+      let json = await response.json();
+      return json.main.temp;
+    }
+    throw new Error(response.status);
 };
 
-search();
+console.log(search('santa teresita', 'imperial'));
