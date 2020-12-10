e10464f96b4226f40b26c880f6259ad36a2f5c6c
EstebanP <epinczinger@gmail.com>
2020-12-10 12:17:28 -0300

    add show results function

--- src/components/form.js
+++
@@ -1,2 +0,0 @@
-import { weatherInfo } from "./api";
-
--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-import './style/style.scss';
-import { weatherInfo, infoAPI} from './components/api';
+import "./style/style.scss";
+import { weatherInfo, infoAPI } from "./components/api";
+import showResults from "./components/pagecontent";
 
-
-const div = document.getElementById('div');
-const p = document.createElement('p');
-p.textContent = '???';
-
+const div = document.getElementById("div");
 div.appendChild(p);
 
-
-const btn = document.getElementById('submit-btn');
-    btn.addEventListener('click', (event) => {
-      processForm();
+const btn = document.getElementById("submit-btn");
+btn.addEventListener("click", (event) => {
+  processForm();
 });
 
-const processForm = () => {
-        const city = document.getElementById('input-city').value;
-        const radios = document.getElementsByClassName("form-check-input");
-        let unit = ''
-        
-        for (let i = 0; i < radios.length; i++) {
-            if (radios[i].checked) {
-                unit = radios[i].value;
-            };
+async function processForm() {
+  const city = document.getElementById("input-city").value;
+  const radios = document.getElementsByClassName("form-check-input");
+  let unit = "";
 
-    console.log(weatherInfo(city, unit));
-            };
-          };
- 
-//        async const infoArray = await weatherInfo(city, unit);
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      unit = radios[i].value;
+    };
+  };
 
-//        console.log(infoArray);
-//         city = '';
-//         radios= '';
-//         unit='';
+  let results = await weatherInfo(city, unit);
 
-//     });
+  showResults(results)
+
+};
+
+const showResults = (obj) => {
+  
+  const searchResults = document.getElementById("search-results");
+  const resultContainer = document.createElement("div");
+  resultContainer.classList.add("result-container");
+  resultContainer.innerHTML = `<div><b>City: </b>${obj.city}, ${obj.country}</div>
+  <div><b>Weather: </b>${obj.weather}</div>
+  <div><b>Temperature: </b>${obj.temperature} degrees</div>`;
+  searchResults.appendChild(resultContainer);
+
+};
+
