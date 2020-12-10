dd70c37355660ac6553b34409aa22b1b00b1c213
EstebanP <epinczinger@gmail.com>
2020-12-10 12:32:06 -0300

    fix eslint errors

--- src/components/api.js
+++ src/components/api.js
@@ -1,14 +1,13 @@
-const infoAPI = async (city, units='metric') => {
-    const apiKey = 'f8584d3f19d9882aec61e1e892543fe7';
-    // city = 'las toninas';
-    let response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`
-    )
-    if (response.status == 200) {
-      let info = await response.json();
-      return info;
-    }
-    throw new Error(response.status);
+const infoAPI = async (city, units = 'metric') => {
+  const apiKey = 'f8584d3f19d9882aec61e1e892543fe7';
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`,
+  );
+  if (response.status === 200) {
+    const info = await response.json();
+    return info;
+  }
+  throw new Error(response.status);
 };
 
 const weatherInfo = async (city, units) => {
@@ -23,5 +22,4 @@ const weatherInfo = async (city, units) => {
   return filteredInfo;
 };
 
-export {infoAPI, weatherInfo};
-
+export { infoAPI, weatherInfo };
--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
-import "./style/style.scss";
-import { weatherInfo, infoAPI } from "./components/api";
-import showResults from "./components/pagecontent";
-
-const div = document.getElementById("div");
-div.appendChild(p);
-
-const btn = document.getElementById("submit-btn");
-btn.addEventListener("click", (event) => {
-  processForm();
-});
-
-async function processForm() {
-  const city = document.getElementById("input-city").value;
-  const radios = document.getElementsByClassName("form-check-input");
-  let unit = "";
-
-  for (let i = 0; i < radios.length; i++) {
-    if (radios[i].checked) {
-      unit = radios[i].value;
-    };
-  };
-
-  let results = await weatherInfo(city, unit);
-
-  showResults(results)
-
-};
+import './style/style.scss';
+import { weatherInfo } from './components/api';
 
 const showResults = (obj) => {
-  
-  const searchResults = document.getElementById("search-results");
-  const resultContainer = document.createElement("div");
-  resultContainer.classList.add("result-container");
+  const searchResults = document.getElementById('search-results');
+  const resultContainer = document.createElement('div');
+  resultContainer.classList.add('result-container');
   resultContainer.innerHTML = `<div><b>City: </b>${obj.city}, ${obj.country}</div>
   <div><b>Weather: </b>${obj.weather}</div>
   <div><b>Temperature: </b>${obj.temperature} degrees</div>`;
   searchResults.appendChild(resultContainer);
-
 };
 
+async function processForm() {
+  const city = document.getElementById('input-city').value;
+  const radios = document.getElementsByClassName('form-check-input');
+  let unit = '';
+
+  for (let i = 0; i < radios.length; i += 1) {
+    if (radios[i].checked) {
+      unit = radios[i].value;
+    }
+  }
+
+  const results = await weatherInfo(city, unit);
+
+  showResults(results);
+}
+
+const btn = document.getElementById('submit-btn');
+btn.addEventListener('click', () => {
+  processForm();
+});
